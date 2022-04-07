d83fd83c98a6f0d3611f85a5290e943f964f18dc
Malek Fahad Amiree <98859571+fahdamiree@users.noreply.github.com>
2022-04-06 22:34:43 -0700

    create by amiree in amiree

---
+++ src/mudaser_rahimi/MalekAmiree.java
@@ -0,0 +1,11 @@
+package mudaser_rahimi;
+
+public class MalekAmiree {
+    public static void main(String[] args) {
+
+
+        System.out.println("malek fahd amiree");
+
+        // how r u Mudaser
+    }
+}
